7b3e151fffc80e74c908e1db5c048c2149a8451e
jose antonio camacho martinez <joseantonio.camacho.martinez@alumnos.fesac.es>
2018-12-26 12:36:12 +0100

    Primeros pasos
    Codigo Fuente

---
+++ Primeros pasos C/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeros_pasos_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // este es un programa de prueba
+            /*comentarios personalizados*/
+
+            Console.WriteLine("Hola mundo");
+
+            Console.ReadKey();
+
+            /*Tipos de Datos*/
+
+            int i = 0;
+            Decimal x = 0.0m;
+            float f = 0.0f;
+            double d = 0.0D;
+
+
+        }
+    }
+}
